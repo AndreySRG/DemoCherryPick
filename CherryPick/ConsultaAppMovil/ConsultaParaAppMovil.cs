015e045f5781afc1bc849d3d02295b013ea3051b
Andrey Ramos <aramos@cruxconsultores.com>
2023-06-06 14:39:06 -0600

    Consulta de viajes usuario con paginacion

--- CherryPick/ConsultaAppMovil/ConsultaParaAppMovil.cs
+++ CherryPick/ConsultaAppMovil/ConsultaParaAppMovil.cs
@@ -5,5 +5,7 @@ namespace ConsultaAppMovil
     public class ConsultaParaAppMovil
     {
         public void TraerViajesDelusuarioParaElApp() { }
+
+        public void PaginarConsulta() { }
     }
 }
