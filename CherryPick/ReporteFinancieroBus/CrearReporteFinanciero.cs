37b6f33b0c327dc17ff4efeb87003124d916efd7
Andrey Ramos <aramos@cruxconsultores.com>
2023-06-06 14:13:19 -0600

    Se agregan calculos de comision para reporte financiero

--- CherryPick/ReporteFinancieroBus/CrearReporteFinanciero.cs
+++ CherryPick/ReporteFinancieroBus/CrearReporteFinanciero.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ReporteFinancieroBus
 {
-    internal class CrearReporteFinanciero
+    public class CrearReporteFinanciero
     {
+        public void ObtenerDatos() { }
+        public void CrearCalculosDeComision() { }
     }
 }
